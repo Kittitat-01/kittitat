7135992123b0cac0392768a60010b59ee47d094f
kittitat-01 <kittitatl60@nu.ac.th>
2020-08-26 23:52:00 +0700

    folder perpared

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
-
 const config = require('../config/config')
 const db = {}
 const sequelize = new Sequelize (
@@ -10,16 +9,16 @@ const sequelize = new Sequelize (
     config.db.password,
     config.db.options
 )
-fs.readdirSync().filter().forEach()
+ 
 fs.readdirSync(__dirname)
     .filter((file) =>
         file !== 'index.js'
- )
+    )
     .forEach((file) => {
         const model = sequelize.import(path.join(__dirname, file))
         db[model.name] = model
     }
- )
- db.sequelize = sequelize
- db.Sequelize = Sequelize
- module.exports = db
+)
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+module.exports = db
